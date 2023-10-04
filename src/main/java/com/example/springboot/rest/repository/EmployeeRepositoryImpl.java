23a17cf246bb3275f424fbd299d056c0296a460a
Bondar Anton <bondar8524567913@gmail.com>
2023-10-04 16:44:57 +0300

    hibernate solutions replaced with JPA

--- src/main/java/com/example/springboot/rest/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/springboot/rest/repository/EmployeeRepositoryImpl.java
@@ -4,9 +4,9 @@ package com.example.springboot.rest.repository;
 import com.example.springboot.rest.model.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+
+import jakarta.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,23 +20,24 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
     private EntityManager manager;
 
     public List<Employee> getAllEmployees() {
-       Query<Employee> query = manager.unwrap(Session.class).createQuery("from Employee ", Employee.class);
+
+       Query query = manager.createQuery("from Employee ");
        return query.getResultList();
     }
 
     public void saveEmployee(Employee employee) {
-        manager.unwrap(Session.class).saveOrUpdate(employee);
+        manager.merge(employee);
     }
 
    public Optional<Employee> getEmployeeById(int id) {
-      Optional<Employee> employee =  Optional.ofNullable(manager.unwrap(Session.class).get(Employee.class,id));
+      Optional<Employee> employee =
+              Optional.ofNullable(manager.find(Employee.class,id));
       return employee;
     }
 
     public void deleteEmployeeById(int id) {
         Optional<Employee>  employee = getEmployeeById(id);
-            manager.unwrap(Session.class).
-            remove(employee.orElseThrow(
+            manager.remove(employee.orElseThrow(
             ()->  new EntityNotFoundException("Its impossible to delete an employee with ID = "+ id)));
 
     }
